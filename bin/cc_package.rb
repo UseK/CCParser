8ccd5409d2f0d920cc278802ef9bfc19901d300a
UseK <use.k.james@gmail.com>
2013-04-18 22:10:11 +0900

    トップディレクトリとボトムディレクトリの処理をいずれ同時に行うため、tempfileを返すモジュールに変更

--- bin/cc_package.rb
+++ bin/cc_package.rb
@@ -3,6 +3,7 @@ $LOAD_PATH << File.dirname(__FILE__) + "/../lib"
 require "pp"
 require "pathname"
 require "parser"
+require "tempfile"
 module PackageParser
  
   # changerを元にidを書き換えたファイルを出力する
@@ -10,41 +11,52 @@ module PackageParser
   # file_path :: 入力するファイルのパス
   # changer :: 変更するidをまとめたハッシュの配列
   # キーが変更前、値が変更後のid
-  def self.parse file_path, changer, root_regexp
+  def self.output_cc_file input_file_path, changer
+    output_file = Tempfile.new("top")
     section = []
-    out_file = File.open(file_path + "_top", "w")
-    File.foreach(file_path) do |line|
-      out_file.print @alt_line
+    File.foreach(input_file_path) do |line|
       case line
       when /^#begin{(.+)}/
         section.push $1
-        @alt_line = line
+        output_file.print line
         next
       when /^#end{(.+)}/
         section.pop
-        @alt_line = line
+        output_file.print line
         next
       end
 
       case section
       when ["file description"]
-        @alt_line = sub_id line, changer
+        output_file.print sub_id line, changer
       when ["clone", "set"]
-        @alt_line = sub_id line, changer
+        output_file.print sub_id line, changer
       else
-        @alt_line = line
+        output_file.print line
       end
     end
-    out_file.print @alt_line
+    output_file.rewind
+    output_file
   end
 
-  def sub_id line, changer
+  def self.sub_id line, changer
     id = line[/^(\d+\.\d+)/, 1]
     line.gsub(/^\d+\.\d+/, changer[id])
   end
 
+  def self.gen_changer input_file
+    file_description, clone = Parser.parse input_file
+    top_arr = []
+    root_regexp = /#{file_description.root_directory}/
+      file_description.each do |unit|
+      dir = unit.path.gsub(root_regexp, "")[/^(\w+)\//, 1]
+      top_arr << dir
+      end
+    top_arr.uniq!
+    PackageParser.make_changer file_description, root_regexp, top_arr
+  end
 
-  def make_changer file_description, root_regexp, top_arr
+  def self.make_changer file_description, root_regexp, top_arr
     changer = {}
     i = 0
     new_pakcage = ""
@@ -63,18 +75,11 @@ module PackageParser
 end
 
 if $0 == __FILE__
-  input_file = ARGV[0] || "./sample_data/ff"
-  include Parser
-  file_description, clone = Parser.parse input_file
-  include PackageParser
-  top_arr = []
-  root_regexp = /#{file_description.root_directory}/
-  file_description.each do |unit|
-    dir = unit.path.gsub(root_regexp, "")[/^(\w+)\//, 1]
-    top_arr << dir
-  end
-  top_arr.uniq!
-  changer = PackageParser.make_changer file_description, root_regexp, top_arr
-  pp changer
-  PackageParser.parse input_file, changer, root_regexp
+  input_file_path = ARGV[0] || "./sample_data/ff"
+  changer = PackageParser.gen_changer input_file_path
+  output_file = PackageParser.output_cc_file input_file_path, changer
+  File.open("#{input_file_path}_top", "w").print output_file.read
+#  output_file.each do |line|
+#    puts line
+#  end
 end
